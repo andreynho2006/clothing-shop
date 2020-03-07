f859e3614ae370af66bf098ff33dc171b9d71ae5
andreynho2006 <andreynho2006@gmail.com>
2020-03-07 14:40:13 +0200

    create a charge function from stripe package to handle post to /payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_lxJLcrdHTRiGfcKof0FvU2nV00iZbYp2Wd';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesuful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('Payment succesuful!');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There is a issue with your paymen t. Please sure you use the provided credit card'
+        );
+      });
   };
 
   return (
